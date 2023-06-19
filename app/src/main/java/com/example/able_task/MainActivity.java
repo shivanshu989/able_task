19bccf88be06696be1918f12255e3baa99e9b9d5
shivanshu989 <81761392+shivanshu989@users.noreply.github.com>
2023-06-19 16:36:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/able_task/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.able_task;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.able_task.databinding.ActivityMainBinding;
+import com.google.android.material.textview.MaterialTextView;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    EditText email, password;
+    Button login, signup;
+    TextView temp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp.append(email.getText());
+            }
+        });
+    }
+}
